d6d0fb2275baa998bdf60bb489953d29ebd005dc
edvard01 <frejaclueless@gmail.com>
2024-02-07 11:22:09 +0100

    Made print console and print DOM into functions that works for all gets, added get for dogs, books and employees, added filter functions for books based on pages, and employees based on attending and attending with allergy

--- js/index.js
+++ js/index.js
@@ -4,26 +4,112 @@ async function getAllPokemon() {
       "https://majazocom.github.io/Data/pokemons.json"
     );
     const data = await response.json();
-    renderPokemonConsole(data);
-    renderPokemonDOM(data);
+
+    const array = [];
+    data.forEach((item) => {
+      array.push(item.name);
+    });
+    renderListConsole(array);
+    renderListDOM(array, ".pokemon-list");
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
-function renderPokemonConsole(pokemon) {
-  for (let i = 0; i < pokemon.length; i++) {
-    console.log(pokemon[i].name);
+async function getAllDogs() {
+  try {
+    const response = await fetch("https://majazocom.github.io/Data/dogs.json");
+    const data = await response.json();
+
+    const array = [];
+    data.forEach((item) => {
+      array.push(item.name);
+    });
+    renderListConsole(array);
+    renderListDOM(array, ".dogs-list");
+  } catch (error) {
+    console.error("Error:", error);
   }
-  return pokemon;
 }
 
-function renderPokemonDOM(pokemon) {
-  const list = document.querySelector(".pokemon-list");
+async function getAllBooks() {
+  try {
+    const response = await fetch("https://majazocom.github.io/Data/books.json");
+    const data = await response.json();
 
-  pokemon.forEach((item) => {
-    list.innerHTML += `<h4>${item.name}</h4>`;
+    const array = [];
+    data.forEach((item) => {
+      array.push(item.title);
+    });
+    renderListDOM(filterBooks(data, 500), ".books-list");
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
+
+async function getAllEmployees() {
+  try {
+    const response = await fetch(
+      "https://majazocom.github.io/Data/attendees.json"
+    );
+    const data = await response.json();
+    const employeesAttended = filterEmployees(data);
+    const employeesAttendedWithAllergy = filterEmployeesAllergy(data);
+    renderListDOM(employeesAttended, ".attending-list");
+    renderListDOM(employeesAttendedWithAllergy, ".attending-with-allergy-list");
+  } catch (error) {
+    console.error("Error", error);
+  }
+}
+
+function renderListConsole(list) {
+  for (let i = 0; i < list.length; i++) {
+    console.log(list[i]);
+  }
+  return list;
+}
+
+function renderListDOM(list, className) {
+  const htmlElement = document.querySelector(className);
+
+  list.forEach((item) => {
+    htmlElement.innerHTML += `<h4>${item}</h4>`;
   });
 }
 
+function filterBooks(books, pageNr) {
+  const list = [];
+  books.forEach((book) => {
+    if (book.pages < pageNr) {
+      list.push(book.title);
+    }
+  });
+  return list;
+}
+
+function filterEmployees(employees) {
+  const list = [];
+  employees.forEach((employee) => {
+    if (employee.attending) {
+      list.push(employee.name);
+    }
+  });
+
+  return list;
+}
+
+function filterEmployeesAllergy(employees) {
+  const list = [];
+  employees.forEach((employee) => {
+    if (employee.allergies.length !== 0 && employee.attending) {
+      list.push(employee.name);
+    }
+  });
+
+  return list;
+}
+
 getAllPokemon();
+getAllDogs();
+getAllBooks();
+getAllEmployees();
